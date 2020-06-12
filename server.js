e3aa34cf6cedc4de8b5463d04ff00e2070809635
FinnWalker <finn3walker@gmail.com>
2020-06-12 17:09:50 +0100

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/iframe', (req, res) => {
-    res.setHeader("Content-Security-Policy", "frame-ancestors none");
+    res.setHeader("Content-Security-Policy", "frame-ancestors self");
     res.send("You win");    
 })
 
